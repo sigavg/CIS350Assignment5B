a1803315112ac03b4bfed15c94c628dd8fda49af
sigavg <lesimplu@gmail.com>
2022-03-10 00:19:52 -0600

    Up to future weapons pack

---
+++ CIS350Assignment5B/Assets/SampleScenes/Scenes/MyFirstPersonPlayer/Scripts/MouseLook.cs
@@ -0,0 +1,41 @@
+﻿/* Luke Lesimple
+ * Assignment 5B
+ * Mouse controls looking around
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public GameObject player;
+    private float verticalLookRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        player.transform.Rotate(Vector2.up * mouseX);
+
+        verticalLookRotation -= mouseY;
+
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(verticalLookRotation, 0f, 0f);
+    }
+
+    private void OnApplicationFocus(bool focus)
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
---
+++ CIS350Assignment5B/Assets/SampleScenes/Scenes/MyFirstPersonPlayer/Scripts/Playermovement.cs
@@ -0,0 +1,60 @@
+﻿/* Luke Lesimple
+ * Assignment 5B
+ * controls player movement
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+
+    public Vector3 velocity;
+    public float gravity = -9.81f;
+    public float gravityMultiplier = 2f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public bool isGrounded;
+
+    public float jumpHeight = 3f;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        gravity *= gravityMultiplier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position,
+            groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
