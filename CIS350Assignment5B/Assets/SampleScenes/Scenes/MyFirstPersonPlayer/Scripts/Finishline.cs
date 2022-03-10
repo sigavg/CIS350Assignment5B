b01e3724b79e0d37e8a1be8496acddd2e743e3c7
sigavg <lesimplu@gmail.com>
2022-03-10 15:59:38 -0600

    Finished

---
+++ CIS350Assignment5B/Assets/SampleScenes/Scenes/MyFirstPersonPlayer/Scripts/Finishline.cs
@@ -0,0 +1,32 @@
+﻿/* Luke Lesimple
+ * Project 5B
+ * Finish line script
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Finishline : MonoBehaviour
+{
+    public Canvas finishCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        finishCanvas.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        finishCanvas.enabled = true;
+    }
+
+}
