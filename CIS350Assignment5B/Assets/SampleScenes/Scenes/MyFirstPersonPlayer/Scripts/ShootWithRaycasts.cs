d687fc380f62332cfa155df205849f6b65febfbf
sigavg <lesimplu@gmail.com>
2022-03-10 00:39:01 -0600

    up to particle effects

---
+++ CIS350Assignment5B/Assets/SampleScenes/Scenes/MyFirstPersonPlayer/Scripts/ShootWithRaycasts.cs
@@ -0,0 +1,43 @@
+﻿/* Luke Lesimple
+ * Assignment 5B
+ * adds shooting functionality
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootWithRaycasts : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")) { Shoot(); }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hitInfo;
+        if ( Physics.Raycast(cam.transform.position,
+            cam.transform.forward, out hitInfo, range) )
+        {
+            Debug.Log(hitInfo.transform.gameObject.name);
+
+            Target target = hitInfo.transform.gameObject.GetComponent<Target>();
+
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}
---
+++ CIS350Assignment5B/Assets/SampleScenes/Scenes/MyFirstPersonPlayer/Scripts/Target.cs
@@ -0,0 +1,33 @@
+﻿/* Luke Lesimple
+ * Assignment 5B
+ * Target behavior
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health = 50f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
